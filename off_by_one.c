be6a52ed6c2a666d2048d4776e17947e5ec34518
ImanJabaliJasim <imanjjassim@gmail.com>
2025-05-24 17:17:16 +0300

    Update off_by_one.c

--- off_by_one.c
+++ off_by_one.c
@@ -1,8 +1,15 @@
-int len = 0, total = 0;
-while(1){
+int length = 0, sum = 0;
+while (1) {
     fgets(buff1, MAX_SIZE, stdin);
-    int len =  strnlen(buff1, MAX_SIZE);
-    total += len;
-    if(total <= MAX_SIZE) strncat(buff2, buff1, len);
-    else break;
+    int len = strnlen(buff1, MAX_SIZE);
+    if (sum + len < MAX_SIZE) {
+        strncat(buff2, buff1, len);
+        sum += len;
+    } else {
+        int remaining = MAX_SIZE - sum - 1;
+        if (remaining > 0) {
+            strncat(buff2, buff1, remaining);
+        }
+        break;
+    }
 }
