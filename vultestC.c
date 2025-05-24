9a063fd50af5493090e544ac329d59a38af48830
iman <imanjjassim@gmail.com>
2025-05-24 16:21:26 +0300

    add a correct C program

---
+++ vultestC.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+
+#define PASSWORD "secret123"
+
+int main() {
+    char userPass[50];
+
+    printf("Enter the password:\n");
+    fgets(userPass, sizeof(userPass), stdin);
+    //gets(userPass);
+
+    if(strncmp(userPass, PASSWORD, 9) == 0) {
+        printf("PASSWORD VERIFIED\n");
+    } else {
+        printf("ACCESS DENIED\n");
+    }
+
+    return 0;
+}
